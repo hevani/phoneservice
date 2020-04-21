9bdfafa6f90bdeec7cb65c8f92382e67d6a110a8
hevani <hayagreev.evani@dlhcorp.com>
2020-04-20 21:02:26 -0400

    removed unused import

--- src/main/java/com/phoneservice/demo/PhoneServiceApplication.java
+++ src/main/java/com/phoneservice/demo/PhoneServiceApplication.java
@@ -2,9 +2,8 @@ package com.phoneservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class PhoneServiceApplication {
 
 	public static void main(String[] args) {
